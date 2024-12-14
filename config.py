b6994baa8e05916b2efcb2c6bf11986c7d5146f6
DaniilMpala <fegffs@gmail.com>
2024-12-14 11:34:15 +0300

    Фикс контекстного меню

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://kate:password123@localhost/plants_shop'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://username:you16732berg7FG3g1^@65.21.56.209/plants_shop'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'secret'
